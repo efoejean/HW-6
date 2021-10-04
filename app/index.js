defbbfd59193151d7dc96335cc3fd364ce0d2733
efoejean <efoejean@gmail.com>
2021-10-03 23:12:00 -0500

    Delete the console to test the file in the browser

--- app/index.js
+++ app/index.js
@@ -7,9 +7,6 @@ const highestMileageVehicle = data
   .sort((a, b) => b.mileage - a.mileage)[0];
 
 fs.writeFile("highestMileageVehicle.Json", String(highestMileageVehicle));
-fs.readFile("./highestMileageVehicle.Json", "utf8").then((contents) => {
-  console.log(contents);
-});
 
 const totalMileage = data.reduce((total, currentPerson) => {
   total += currentPerson.vehicles.reduce((personTotal, currentVehicle) => {
@@ -21,26 +18,19 @@ const totalMileage = data.reduce((total, currentPerson) => {
 }, 0);
 
 fs.writeFile("totalMileage.Json", String(totalMileage));
-fs.readFile("./totalMileage.Json", "utf8").then((contents) => {
-  console.log(contents);
-});
+
 const yahooEmails = data
   .filter(({ email }) => email.endsWith("yahoo.com"))
   .map(({ email }) => email);
 
 fs.writeFile("yahooEmails.Json", String(yahooEmails));
 
-fs.readFile("./yahooEmails.Json", "utf8").then((contents) => {
-  console.log(contents);
-});
 const hiMileageVehicles = data
   .map(({ vehicles }) => vehicles.filter(({ mileage }) => mileage >= 36000))
   .flat();
 
 fs.writeFile("hiMileageVehicles.Json", String(hiMileageVehicles));
-fs.readFile("./hiMileageVehicles.Json", "utf8").then((contents) => {
-  console.log(contents);
-});
+
 const totalMileage4IL = data
   .map(({ vehicles }) => vehicles.filter(({ st }) => st === "Illinois"))
   .flat()
@@ -50,9 +40,6 @@ const totalMileage4IL = data
   }, 0);
 
 fs.writeFile("totalMileage4IL.Json", String(totalMileage4IL));
-fs.readFile("./totalMileage4IL.Json", "utf8").then((contents) => {
-  console.log(contents);
-});
 
 const app = express();
 
